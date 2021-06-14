79a13a4df0987ccc525379564a00a69321b1d8f5
Md. Faizur Rahman Khan <khan.nayeem8662@gmail.com>
2021-06-14 18:02:45 +0600

    code splitting added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "antd/dist/antd.css";
 // import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TodoList from "./components/TodoList";
-import Login from "./components/loginForm";
+// import TodoList from "./components/TodoList";
+// import Login from "./components/loginForm";
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Registration from "./components/Registration";
+// import Home from "./components/Home/Home";
+// import Registration from "./components/Registration";
+import loadingGif from "./Assets/loading.gif";
+
+const TodoList = lazy(() => import("./components/TodoList"));
+const Login = lazy(() => import("./components/loginForm"));
+
+const Home = lazy(() => import("./components/Home/Home"));
+const Registration = lazy(() => import("./components/Registration"));
+
 function App() {
   return (
     <div>
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/registration">
-            <Registration />
-          </Route>
-          <Route path="/todoList">
-            <TodoList />
-          </Route>
+          <Suspense
+            fallback={
+              <div>
+                <img src={loadingGif} alt="" />
+              </div>
+            }
+          >
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/registration">
+              <Registration />
+            </Route>
+            <Route path="/todoList">
+              <TodoList />
+            </Route>
+          </Suspense>
         </Switch>
       </Router>
     </div>
